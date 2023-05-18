7bc275d292b72ab328d9e51ff07edce465f804ce
Alison Bartsch <abartsch@andrew.cmu.edu>
2023-05-18 16:27:52 -0400

    added visualization capabilities for cluster-based dynamics model

--- visualize.py
+++ visualize.py
@@ -63,6 +63,100 @@ Reconstructing next state with state's centers
 
 # assert False
 
+"""
+Visualize center cluster dynamics model
+"""
+# define the action space and dynamics loss type
+path = '/home/alison/Clay_Data/Fully_Processed/All_Shapes'
+dvae_path = 'experiments/dvae/ShapeNet55_models/test_dvae/ckpt-best.pth'
+word_dynamics_path = 'dvae_dynamics_experiments/exp8_twonetworks_ce'
+center_dynamics_path = 'dvae_dynamics_experiments/exp10_centroid`'
+
+# load the dvae model
+config = cfg_from_yaml_file('cfgs/Dynamics/dvae.yaml')
+config=config.config
+dvae = builder.model_builder(config)
+builder.load_model(dvae, dvae_path, logger = 'dvae_testclay')
+device = torch.device('cuda')
+dvae.to(device)
+dvae.eval()
+
+# load the checkpoint
+checkpoint = torch.load(center_dynamics_path + '/checkpoint', map_location=torch.device('cpu'))
+word_checkpoint = torch.load(word_dynamics_path + '/checkpoint', map_location=torch.device('cpu'))
+dynamics_network = checkpoint['dynamics_network'].to(device)
+word_dynamics = word_checkpoint['dynamics_network'].to(device)
+
+# initialize the dataset
+dataset = DemoWordDataset(path, 'shell_scaled', dvae)
+
+# do we also predict the new vocab?
+predict_words = False
+
+# test_samples = [0, 6, 100, 3000, 5067, 2048, 2678, 3333, 6983, 222, 468, 172]
+
+# samples with the largest s vs ns difference
+test_samples = [0, 60, 120, 180, 240, 300, 360, 420, 480, 3000, 3060, 3120]
+for index in test_samples:
+    state, next_state, action, _ = dataset.__getitem__(index)
+
+    state = torch.unsqueeze(state, 0)
+    next_state = torch.unsqueeze(next_state, 0)
+    action = torch.unsqueeze(action, 0)
+
+    # create gt next state point cloud [BLUE]
+    ns = next_state.squeeze().detach().cpu().numpy()
+    og_pcl = o3d.geometry.PointCloud()
+    og_pcl.points = o3d.utility.Vector3dVector(ns)
+    og_colors = np.tile(np.array([0, 0, 1]), (len(ns),1))
+    og_pcl.colors = o3d.utility.Vector3dVector(og_colors)
+    # o3d.visualization.draw_geometries([og_pcl])
+
+    # create gt state point cloud [GREEN]
+    s = state.squeeze().detach().cpu().numpy()
+    s_pcl = o3d.geometry.PointCloud()
+    s_pcl.points = o3d.utility.Vector3dVector(s)
+    s_colors = np.tile(np.array([0, 1, 0]), (len(s),1))
+    s_pcl.colors = o3d.utility.Vector3dVector(s_colors)
+
+    state = state.cuda()
+    next_state = next_state.cuda()
+    action = action.cuda()
+
+    z_state, neighborhood, center, logits = dvae.encode(state) 
+    ns_center_pred = dynamics_network(center, action).to(device)
+
+    if predict_words:
+    # --------- calculate new vocab ---------
+        # TODO: INCLUDE SOME CLUSTERING/REORDERING OF NS_CENTER_PRED TO MATCH THE VOCAB ORDER OF THOSE NEARBY
+        group_center = ns_center_pred.squeeze()
+        vocab = z_state.squeeze()
+        action = torch.tile(action, (vocab.size()[0], 1))
+
+        ns_logits = word_dynamics(vocab, group_center, action)
+        ns_logits = torch.unsqueeze(ns_logits, 0)
+        print("\nns_logits.size():", ns_logits.size())
+        latent_sampled = dvae.latent_logits_sample(ns_logits)
+
+        ret_recon_next = dvae.decode(latent_sampled, neighborhood, ns_center_pred, ns_logits, state) #.to(device)
+        recon_pcl = ret_recon_next[1]
+    
+    else:
+        ret_recon_next = dvae.decode(z_state, neighborhood, ns_center_pred, logits, state) #.to(device)
+        recon_pcl = ret_recon_next[1]
+
+    # visualize reconstructed cloud [RED]
+    recon_pcl = recon_pcl.squeeze().detach().cpu().numpy()
+    recon_pcl = np.reshape(recon_pcl, (2048, 3))
+    pcl = o3d.geometry.PointCloud()
+    pcl.points = o3d.utility.Vector3dVector(recon_pcl)
+    pcl_colors = np.tile(np.array([1, 0, 0]), (len(recon_pcl),1))
+    pcl.colors = o3d.utility.Vector3dVector(pcl_colors)
+    # o3d.visualization.draw_geometries([pcl])
+    o3d.visualization.draw_geometries([og_pcl, pcl])
+    # o3d.visualization.draw_geometries([pcl, s_pcl, og_pcl])
+
+assert False
 
 
 """
